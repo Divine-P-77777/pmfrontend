4641d4a2c257001fb67bcfac5d460b8bf3c7f964
Divine-P-77777 <165488162+Divine-P-77777@users.noreply.github.com>
2024-08-29 13:33:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Example: Specifies the output directory
+    sourcemap: true,  // Example: Generates source maps
+    // Add more build options here
+  },
+  server: {
+    port: 3000,  // Example: Defines the development server port
+    open: true,  // Example: Automatically opens the browser on server start
+    // Add more server options here
+  }
 })
